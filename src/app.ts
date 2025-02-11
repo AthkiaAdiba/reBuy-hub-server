8a3c37659a73c47638394a19ad0b13b1b2c884f9
thkiaAdiba <athkiaadiba@gmail.com>
2025-02-11 21:33:20 +0600

    added error handler

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,33 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
+import globalErrorHandler from './app/middlewares/globalErrorhandler';
+import notFound from './app/middlewares/notFound';
+import router from './app/routes';
+import cookieParser from 'cookie-parser';
 const app: Application = express();
 
-// parser
+// app.use(cors({ origin: ['http://localhost:5173'], credentials: true }));
+
+// parsers
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 
-app.get('/', (req: Request, res: Response) => {
+// application routes
+app.use('/api/v1', router);
+
+const test = async (req: Request, res: Response) => {
   res.send('Hello World!');
-});
+};
+
+app.get('/', test);
+
+app.use(globalErrorHandler);
+
+// NOT Found
+app.use(notFound);
 
 export default app;
+
+// it is a http-status
+// npm i @types/http-status
---
+++ src/app/routes/index.ts
@@ -0,0 +1,18 @@
+import { Router } from 'express';
+
+const router = Router();
+
+// const moduleRoutes = [
+// //   {
+// //     path: '/users',
+// //     route: UserRoutes,
+// //   },
+
+// ];
+
+// moduleRoutes.forEach((route) => router.use(route.path, route.route));
+
+// router.use('/user', UserRoutes);
+// router.use('/students', StudentRoutes);
+
+export default router;
